9dfdb1a6059d5fd6eca526b2ea5dfee82d314d8b
anastasiiaxfr <anastasiiaberest@gmail.com>
2025-06-23 12:25:53 +0300

    hw-9

--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,16 +1,31 @@
-import SortableList from '../../2-sortable-list/src/index.js';
+import SortableList from '../../09-tests-for-frontend-apps/2-sortable-list/index.js';
+
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
+import ProductForm0 from '../../08-forms-fetch-api-part-2/1-product-form-v1/index.js';
+
 
 const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ProductForm {
-  constructor (productId) {
+export default class ProductForm extends ProductForm0 {
+  constructor (productId = null) {
+    super(productId);
     this.productId = productId;
   }
 
   async render () {
+    await super.render();
 
+    return this.element;
+  }
+
+  renderImageListContainerElement() {
+
+    let items = super.getListImagesData();
+
+    const sortableList = new SortableList({items});
+
+    this.subElements.imageListContainer.append(sortableList.element);
   }
 }
--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,135 @@
 export default class SortableList {
 
+  element;
+  draggingElement;
+  placeHolderElement;
+
+  constructor({items} = {}) {
+    this.items = items;
+
+    this.render();
+    this.createListeners();
+  }
+
+  render() {
+
+    this.element = document.createElement('ul');
+    this.element.className = 'sortable-list';
+
+    for (let item of this.items) {
+      item.classList.add('sortable-list__item');
+      this.element.append(item);
+    }
+
+    return this.element;
+  }
+
+  handlePointerDown = (event) => {
+
+    const itemElement = event.target.closest('.sortable-list__item');
+    this.draggingElement = itemElement;
+
+    if (itemElement) {
+
+      if (event.target.closest('[data-delete-handle]')) {
+        event.preventDefault();
+        itemElement.remove();
+      }
+
+      if (event.target.closest('[data-grab-handle]')) {
+        event.preventDefault();
+
+        this.shiftX = event.clientX - itemElement.getBoundingClientRect().x;
+        this.shiftY = event.clientY - itemElement.getBoundingClientRect().y;
+
+        this.placeHolderElement = document.createElement('div');
+        this.placeHolderElement.className = "sortable-list__placeholder";
+        itemElement.style.width = itemElement.offsetWidth + "px";
+        itemElement.style.height = itemElement.offsetHeight + "px";
+        this.placeHolderElement.style.width = itemElement.style.width;
+        this.placeHolderElement.style.height = itemElement.style.height;
+
+        itemElement.classList.add("sortable-list__item_dragging");
+        itemElement.after(this.placeHolderElement);
+
+        this.element.append(itemElement);
+
+        this.moveElementAt(event.clientX, event.clientY);
+
+        document.addEventListener('pointermove', this.handlePointerMove);
+
+      }
+    }
+  }
+
+  moveElementAt(clientX, clientY) {
+    this.draggingElement.style.left = clientX - this.shiftX + "px";
+    this.draggingElement.style.top = clientY - this.shiftY + "px";
+  }
+
+  handlePointerMove = (event) => {
+
+    this.moveElementAt(event.clientX, event.clientY);
+
+    const items = this.element.children;
+
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+
+      if (item === this.draggingElement) {
+        return;
+      }
+
+      const { top, bottom, height} = item.getBoundingClientRect();
+
+      if (event.clientY > top && event.clientY < bottom) {
+        if (event.clientY < top + height / 2) {
+          this.movePlaceHolderAt(i);
+        } else {
+          this.movePlaceHolderAt(i + 1);
+        }
+
+        break;
+      }
+    }
+  }
+
+  movePlaceHolderAt(index) {
+    if (this.element.children[index] !== this.placeHolderElement) {
+      this.element.insertBefore(this.placeHolderElement, this.element.children[index]);
+    }
+  }
+
+  handlePointerUp = (event) => {
+
+    document.removeEventListener('pointermove', this.handlePointerMove);
+
+    this.placeHolderElement.replaceWith(this.draggingElement);
+
+    this.draggingElement.classList.remove("sortable-list__item_dragging");
+    this.draggingElement.style.left = "";
+    this.draggingElement.style.top = "";
+    this.draggingElement.style.width = "";
+    this.draggingElement.style.height = "";
+
+  }
+
+  createListeners() {
+    document.addEventListener('pointerdown', this.handlePointerDown);
+    document.addEventListener('pointerup', this.handlePointerUp);
+  }
+
+  destroyListners() {
+    document.removeEventListener('pointerdown', this.handlePointerDown);
+    document.removeEventListener('pointerup', this.handlePointerUp);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.destroyListners();
+    this.remove();
+  }
 }
